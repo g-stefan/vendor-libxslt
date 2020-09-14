43c03ed68947a35e32677f36dce269184932b3f4
Grigore Stefan <g_stefan@yahoo.com>
2020-09-14 13:43:52 +0300

    update

--- port/build.msvc.sign.cmd
+++ port/build.msvc.sign.cmd
@@ -6,7 +6,6 @@ rem Created by Grigore Stefan <g_stefan@yahoo.com>
 echo -^> sign vendor-libxslt
 
 pushd build\bin
-echo %CD%
 for /r %%i in (*.dll) do call grigore-stefan.sign "LibXSLT" "%%i"
 for /r %%i in (*.exe) do call grigore-stefan.sign "LibXSLT" "%%i"
 popd
