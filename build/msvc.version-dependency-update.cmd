ba1aa3e886f54c1dda0795220d6a41f3df158ce2
Grigore Stefan <g_stefan@yahoo.com>
2021-08-09 15:41:47 +0300

    Update

---
+++ build/msvc.version-dependency-update.cmd
@@ -0,0 +1,9 @@
+@echo off
+rem Public domain
+rem http://unlicense.org/
+rem Created by Grigore Stefan <g_stefan@yahoo.com>
+
+echo -^> version-dependency-update vendor-libxslt
+
+xyo-cc --update-version-dependency vendor-libxslt
+
