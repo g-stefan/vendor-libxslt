71c9ca85242963f22b938499890dc9f81bad5e9a
Grigore Stefan <g_stefan@yahoo.com>
2024-01-02 22:53:37 +0200

    Update

--- fabricare/make.js
+++ fabricare/make.js
@@ -1,6 +1,6 @@
 // Created by Grigore Stefan <g_stefan@yahoo.com>
 // Public domain (Unlicense) <http://unlicense.org>
-// SPDX-FileCopyrightText: 2022-2023 Grigore Stefan <g_stefan@yahoo.com>
+// SPDX-FileCopyrightText: 2022-2024 Grigore Stefan <g_stefan@yahoo.com>
 // SPDX-License-Identifier: Unlicense
 
 Fabricare.include("vendor");
@@ -25,28 +25,28 @@ Shell.mkdirRecursivelyIfNotExists("temp");
 Shell.mkdirRecursivelyIfNotExists("temp/cmake");
 
 if (!Shell.fileExists("temp/build.config.flag")) {
-	Shell.setenv("CC","cl.exe");
-	Shell.setenv("CXX","cl.exe");
+	Shell.setenv("CC", "cl.exe");
+	Shell.setenv("CXX", "cl.exe");
 
-	cmdConfig="cscript configure.js";
-	cmdConfig+=" zlib=yes";
-	cmdConfig+=" vcmanifest=yes";
-	cmdConfig+=" debug=no";
-	cmdConfig+=" static=no";
-	cmdConfig+=" xslt_debug=yes";
-	cmdConfig+=" bindir="+Shell.realPath(Shell.getcwd())+"\\output\\bin";
-	cmdConfig+=" incdir="+Shell.realPath(Shell.getcwd())+"\\output\\include";
-	cmdConfig+=" libdir="+Shell.realPath(Shell.getcwd())+"\\output\\lib";
-	cmdConfig+=" sodir="+Shell.realPath(Shell.getcwd())+"\\output\\bin";
+	cmdConfig = "cscript configure.js";
+	cmdConfig += " zlib=yes";
+	cmdConfig += " vcmanifest=yes";
+	cmdConfig += " debug=no";
+	cmdConfig += " static=no";
+	cmdConfig += " xslt_debug=yes";
+	cmdConfig += " bindir=" + Shell.realPath(Shell.getcwd()) + "\\output\\bin";
+	cmdConfig += " incdir=" + Shell.realPath(Shell.getcwd()) + "\\output\\include";
+	cmdConfig += " libdir=" + Shell.realPath(Shell.getcwd()) + "\\output\\lib";
+	cmdConfig += " sodir=" + Shell.realPath(Shell.getcwd()) + "\\output\\bin";
 
-	runInPath("source\\win32",function(){
+	runInPath("source\\win32", function() {
 		exitIf(Shell.system(cmdConfig));
 	});
 
 	Shell.filePutContents("temp/build.config.flag", "done");
 };
 
-runInPath("source\\win32",function(){
+runInPath("source\\win32", function() {
 	exitIf(Shell.system("nmake /f Makefile.msvc"));
 	exitIf(Shell.system("nmake /f Makefile.msvc install"));
 	exitIf(Shell.system("nmake /f Makefile.msvc clean"));
--- fabricare/vendor.js
+++ fabricare/vendor.js
@@ -1,6 +1,6 @@
 // Created by Grigore Stefan <g_stefan@yahoo.com>
 // Public domain (Unlicense) <http://unlicense.org>
-// SPDX-FileCopyrightText: 2022-2023 Grigore Stefan <g_stefan@yahoo.com>
+// SPDX-FileCopyrightText: 2022-2024 Grigore Stefan <g_stefan@yahoo.com>
 // SPDX-License-Identifier: Unlicense
 
 messageAction("vendor");
@@ -22,23 +22,27 @@ if (Shell.hasEnv("VENDOR_SOURCE_GIT")) {
 	vendorSourceGit = Shell.getenv("VENDOR_SOURCE_GIT");
 };
 
-exitIf(Shell.system("curl --insecure --location " + vendorSourceGit + "/vendor-" + Project.name + "/releases/download/v" + Project.version + "/" + Project.vendor + ".7z --output archive/" + Project.vendor + ".7z"));
-if (Shell.getFileSize("archive/" + Project.vendor + ".7z") > 16) {
+var vendorSourceAuth = "";
+if (Shell.hasEnv("VENDOR_SOURCE_AUTH")) {
+	vendorSourceAuth = Shell.getenv("VENDOR_SOURCE_AUTH");
+};
+
+exitIf(Shell.system("curl --insecure --location " + vendorSourceGit + "/vendor-" + Project.name + "/releases/download/v" + Project.version + "/" + Project.vendor + ".7z "+vendorSourceAuth+" --output archive/" + Project.vendor + ".7z"));
+if (Shell.getFileSize("archive/" + Project.vendor + ".7z") > 32768) {
 	return;
 };
 Shell.removeFile("archive/" + Project.vendor + ".7z");
 
 // Source
 runInPath("archive", function() {
-	webLink = "https://gitlab.gnome.org/GNOME/libxslt/-/archive/v"+Project.version+"/libxslt-v"+Project.version+".tar.gz";
+	webLink = "https://gitlab.gnome.org/GNOME/libxslt/-/archive/v" + Project.version + "/libxslt-v" + Project.version + ".tar.gz";
 	if (!Shell.fileExists(Project.vendor + ".zip")) {
 		exitIf(Shell.system("curl --insecure --location " + webLink + " --output " + Project.vendor + ".tar.gz"));
 	};
 	Shell.system("7z x " + Project.vendor + ".tar.gz -so | 7z x -aoa -si -ttar -o.");
 	Shell.removeFile(Project.vendor + ".tar.gz");
 	Shell.removeFile(Project.vendor + ".7z");
-	Shell.rename("libxslt-v"+Project.version,"libxslt-"+Project.version);
+	Shell.rename("libxslt-v" + Project.version, "libxslt-" + Project.version);
 	exitIf(Shell.system("7z a -mx9 -mmt4 -r- -sse -w. -y -t7z " + Project.vendor + ".7z " + Project.vendor));
 	Shell.removeDirRecursively(Project.vendor);
 });
-
