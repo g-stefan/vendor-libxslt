1b38ed33c7fe7e809f57c3324419002c9be3ccfa
Grigore Stefan <g_stefan@yahoo.com>
2024-08-28 17:07:28 +0300

    Update

--- fabricare/make.js
+++ fabricare/make.js
@@ -25,33 +25,49 @@ Shell.mkdirRecursivelyIfNotExists("temp");
 Shell.mkdirRecursivelyIfNotExists("temp/cmake");
 
 if (!Shell.fileExists("temp/build.config.flag")) {
+
 	Shell.setenv("CC", "cl.exe");
 	Shell.setenv("CXX", "cl.exe");
 
-	cmdConfig = "cscript configure.js";
-	cmdConfig += " zlib=yes";
-	cmdConfig += " vcmanifest=yes";
-	cmdConfig += " debug=no";
-	cmdConfig += " static=no";
-	cmdConfig += " xslt_debug=yes";
-	cmdConfig += " bindir=" + Shell.realPath(Shell.getcwd()) + "\\output\\bin";
-	cmdConfig += " incdir=" + Shell.realPath(Shell.getcwd()) + "\\output\\include";
-	cmdConfig += " libdir=" + Shell.realPath(Shell.getcwd()) + "\\output\\lib";
-	cmdConfig += " sodir=" + Shell.realPath(Shell.getcwd()) + "\\output\\bin";
+	cmdConfig = "cmake";
+	cmdConfig += " ../../source";
+	cmdConfig += " -G \"Ninja\"";
+	cmdConfig += " -DCMAKE_BUILD_TYPE=Release";
+	cmdConfig += " -DCMAKE_INSTALL_PREFIX=" + Shell.realPath(Shell.getcwd()) + "\\output";
 
-	runInPath("source\\win32", function() {
+	if (Fabricare.isDynamic()) {
+		cmdConfig += " -DBUILD_SHARED_LIBS=ON";
+
+		cmdConfig += " -DLIBXSLT_WITH_DEBUGGER=OFF";
+		cmdConfig += " -DLIBXSLT_WITH_PYTHON=OFF";
+		cmdConfig += " -DLIBXSLT_WITH_TESTS=OFF";
+	};
+
+	if (Fabricare.isStatic()) {
+		cmdConfig += " -DBUILD_SHARED_LIBS=OFF";
+		cmdConfig += " -DWIN32_MT_BUILD=ON";
+
+		cmdConfig += " -DLIBXSLT_WITH_DEBUGGER=OFF";
+		cmdConfig += " -DLIBXSLT_WITH_PYTHON=OFF";
+		cmdConfig += " -DLIBXSLT_WITH_TESTS=OFF";
+	};
+
+	runInPath("temp/cmake", function() {
 		exitIf(Shell.system(cmdConfig));
 	});
 
 	Shell.filePutContents("temp/build.config.flag", "done");
 };
 
-runInPath("source\\win32", function() {
-	exitIf(Shell.system("nmake /f Makefile.msvc"));
-	exitIf(Shell.system("nmake /f Makefile.msvc install"));
-	exitIf(Shell.system("nmake /f Makefile.msvc clean"));
+runInPath("temp/cmake", function() {
+	exitIf(Shell.system("ninja"));
+	exitIf(Shell.system("ninja install"));
+	exitIf(Shell.system("ninja clean"));
 });
 
-exitIf(!Shell.copyFile("output/lib/libxslt_a.lib", "output/lib/libxslt.static.lib"));
+if (Fabricare.isStatic()) {
+	exitIf(!Shell.copyFile("output/lib/libexslts.lib", "output/lib/libexslt.lib"));
+	exitIf(!Shell.copyFile("output/lib/libxslts.lib", "output/lib/libxslt.lib"));
+};
 
 Shell.filePutContents("temp/build.done.flag", "done");
